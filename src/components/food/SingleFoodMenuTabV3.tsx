ee6e4c70ccdd07572461fbc0f26e94f3039dec1f
Nabih Attar <nabihattarr@gmail.com>
2026-01-26 10:35:45 +0200

    menu edits

--- src/components/food/SingleFoodMenuTabV3.tsx
+++ src/components/food/SingleFoodMenuTabV3.tsx
@@ -1,51 +1,60 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 interface DataType {
-    id: number
-    thumb: string
-    name: string
-    // price: number
-    priceFull: number
-    leftInfo: string
-    rightInfo: string
+  id: number;
+  thumb: string;
+  name: string;
+  price: number;
+  priceFull: number;
+  leftInfo: string;
+  rightInfo: string;
 }
 
 const SingleFoodMenuTabV3 = ({ data }: { data: DataType }) => {
-    const { id, thumb, name, priceFull, leftInfo, rightInfo } = data
+  const { id, thumb, name, price, priceFull, leftInfo, rightInfo } = data;
 
-    return (
-        <>
-            <li>
-                <div className="thumbnail">
-                    <Image src={`/assets/img/food/${thumb}`} alt="Image Not Found" width={200} height={200} />
-                </div>
-                <div className="content">
-                    <div className="top">
-                        <div className="title">
-                            <h4><Link href={`/shop-single/${id}`}>{name}</Link></h4>
-                        </div>
-                        <div className="price">
-                            {/* <span>${price}</span> */}
-                            <span>L.L{priceFull}</span>
-                        </div>
-                    </div>
-                    <div className="bottom">
-                        <div className="left">
-                            <p>
-                                {leftInfo}
-                            </p>
-                        </div>
-                        <div className="right">
-                            <p>
-                                {rightInfo}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </>
-    );
+  const showPrice = Number(price) > 0;
+  const showPriceFull = Number(priceFull) > 0;
+
+  return (
+    <li>
+      <div className="thumbnail">
+        <Image
+          src={`/assets/img/food/${thumb}`}
+          alt="Image Not Found"
+          width={200}
+          height={200}
+        />
+      </div>
+
+      <div className="content">
+        <div className="top">
+          <div className="title">
+            <h4>
+              <Link href={`/shop-single/${id}`}>{name}</Link>
+            </h4>
+          </div>
+
+          {(showPrice || showPriceFull) && (
+            <div className="price">
+              {showPrice && <span>${price}</span>}
+              {showPriceFull && <span>${priceFull}</span>}
+            </div>
+          )}
+        </div>
+
+        <div className="bottom">
+          <div className="left">
+            <p>{leftInfo}</p>
+          </div>
+          <div className="right">
+            <p>{rightInfo}</p>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default SingleFoodMenuTabV3;
